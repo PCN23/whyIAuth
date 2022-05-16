0eda0789e2a7dfd69413964a7bcb387c0cd224de
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-16 11:40:42 -0700

    added event listener

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { signupUser } from "./fetch-utils.js";
+
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
@@ -6,8 +8,12 @@ const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
-signUpForm.addEventListener('submit', (e) => {
-    (e.preventDefault);
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    console.log({ email: data.get('email'), password: data.get('password') });
+    const user = await signupUser(data.get('email', data.get('password')));
+    console.log(user);
 });
 
 
