2a35c81461c2ccc97c3b36f269a776a018b9ab43
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-16 11:11:38 -0700

    added prevent default

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
+signUpForm.addEventListener('submit', (e) => {
+    (e.preventDefault);
+});
+
+
+
+
+
+
+
+
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://rtlscqxivjinblfrlgls.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0bHNjcXhpdmppbmJsZnJsZ2xzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMDU0MTMsImV4cCI6MTk2Nzg4MTQxM30.00XIP2vqbUttvWfraYlPerWhL6U91pxWMD114J1FM8I';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
